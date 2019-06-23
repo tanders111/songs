ab183451c1e12c9213aedc3a35e43eba16e3cb9e
Todd Anderson <tanders111@gmail.com>
2019-06-23 06:49:42 -0500

    song file location

--- src/server/routes.js
+++ src/server/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-var home = '/Users/Todd/Documents/music/songs'; //'files';
+var home = '/dev/songs/files'; //'files';
 
 router.get('/songs/:name', function (req, res, next) {
 
